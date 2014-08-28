258c9930aa35e8e8d99bba7d4133d6a39107436d
jose <jose@esteves>
2014-08-28 19:40:00 -0300

    Inicio do Projeto. Deus nos abençoe.

---
+++ src/main/java/br/com/greatsoft/greaterp/Main.java
@@ -0,0 +1,12 @@
+package br.com.greatsoft.greaterp;
+
+/**
+ *
+ * @author jose
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Main application");
+    }
+    
+}
